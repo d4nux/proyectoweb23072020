38ec94f192aeac231e5bcc0f8b5a19c3c80940cd
Danny Lopez <dalos00@gmail.com>
2020-07-30 16:43:03 -0500

    Se agregó funcionalidad CRUD

--- public/js/index.js
+++ public/js/index.js
@@ -13,18 +13,104 @@ firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
+var idUsuario = document.getElementById('id');
+var txtname = document.getElementById('name');
+var apellidos = document.getElementById('apellidos');
+var listaDocente = document.getElementById('listaDocente');
+var opcion = document.getElementById('rol');
+
+var btnAgregar = document.getElementById('btnAgregar');
+var btnActualizar = document.getElementById('btnActualizar');
+
 function agregarDatos(user) {
+    leerDatos();
     db.collection("docentes").add({
-        nombre: "Danny",
-        apellido: "Lopez",
-        rol: 0
+        nombre: txtname.value,
+        apellido: apellidos.value,
+        rol: opcion.value
     })
         .then((docRef) => {
             console.log("Document written with ID: ", docRef.id);
-            alert('Datos agregados correctamente', docRef.id)
+            alert('Datos agregados correctamente', docRef.id);
+            limpiarDatos();
         })
         .catch((error) => {
-            console.error("Error adding document: ", error);
+            console.error("Error: ", error);
         });
+    //console.log(`El nombre es: ${txtname.value} y el apellido es: ${apellidos.value}`);
+}
 
+leerDatos();
+
+function leerDatos() {
+    listaDocente.innerHTML = "";
+    btnActualizar.classList.add('d-none');
+
+    db.collection("docentes").get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                listaDocente.innerHTML += `
+                    <tr>
+                        <td>${doc.data().nombre}</td>
+                        <td>${doc.data().apellido}</td>
+                        <td>${doc.data().rol}</td>
+                        <td>
+                            <button onclick="eliminar('${doc.id}')" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                            <button onclick="editar('${doc.id}')" class="btn btn-info"><i class="far fa-edit"></i></button>
+                        </td>
+                    </tr>           
+                `;
+            });
+        })
+        .catch(function (error) {
+            console.log("Error: ", error);
+        });
+}
+
+function eliminar(id) {
+    db.collection("docentes").doc(id).delete()
+        .then(() => {
+            console.log("Documento eliminado");
+            leerDatos();
+        }).catch((error) => {
+            console.error("Error: ", error);
+        });
+}
+
+function editar(id) {
+    btnAgregar.classList.add('d-none');
+    btnActualizar.classList.remove('d-none');
+    db.collection("docentes").doc(id).get()
+        .then((doc) => {
+            idUsuario.value = id;
+            txtname.value = doc.data().nombre;
+            apellidos.value = doc.data().apellido;
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });
+}
+
+function actualizarDatos() {
+    db.collection("docentes").doc(idUsuario.value).update({
+        nombre: txtname.value,
+        apellido: apellidos.value,
+        rol: opcion.value
+    })
+        .then(() => {
+            limpiarDatos()
+            leerDatos();
+            btnActualizar.classList.add('d-none');
+            btnAgregar.classList.remove('d-none');
+            console.log("Document successfully updated!");
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });;
+
+}
+
+function limpiarDatos() {
+    txtname.value = "";
+    apellidos.value = "";
 }
